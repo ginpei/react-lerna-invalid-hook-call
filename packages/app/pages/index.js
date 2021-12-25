e0c891599f7fa2612cb8a1d47154ff0c1c9cb973
Ginpei <ginpei@gmail.com>
2021-12-25 00:06:16 +0000

    use outer hook (error)
    Error: Invalid hook call

--- packages/app/pages/index.js
+++ packages/app/pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useRandom } from '@a/shared';
 
 export default function Home() {
+  const number = useRandom();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +18,10 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <p>
+          Random number: {number}
+        </p>
 
         <p className={styles.description}>
           Get started by editing{' '}
